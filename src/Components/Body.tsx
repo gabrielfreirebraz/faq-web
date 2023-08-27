d87f5a590757ea3edebd0c6ed884e7575ec2a01b
Gabriel Freire Braz <gabrielfreirebraz@gmail.com>
2023-08-27 18:12:41 -0300

    feat: add switchboxes css

--- src/Components/Body.tsx
+++ src/Components/Body.tsx
@@ -21,11 +21,12 @@ export const DivBody = (props: PropsBody) => {
         <Row>
           <Col md={3}></Col>
           <Col md={6}>
-            <DivCard title=""></DivCard>
-            <DivCard title=""></DivCard>
-            <DivCard title=""></DivCard>
-            <DivCard title=""></DivCard>
-            <DivCard title=""></DivCard>
+            <DivCard title="Como saber se tenho acesso ao Portal MIS" id={1}></DivCard>
+            <DivCard title="Vejo erro de usuário ou senha inválido, mas digitei o login corretamente" id={2}></DivCard>
+            <DivCard title="Como saber se meu login de rede está bloqueado" id={3}></DivCard>
+            <DivCard title="Como prosseguir com o login no Portal MIS após resetar a senha de rede" id={4}></DivCard>
+            <DivCard title="Quais os principais erros do AD ao logar na rede" id={5}></DivCard>
+            <DivCard title="O problema persiste e não consigo autenticar no Portal MIS" id={5}></DivCard>
           </Col>
           <Col md={3}></Col>
         </Row>
--- src/Components/Card.css.ts
+++ src/Components/Card.css.ts
@@ -3,5 +3,23 @@ import styled from "styled-components";
 export const StylesCard = styled.div`
   .alert {
     margin: 0.3rem auto;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+  .alert svg,
+  .alert .form-switch,
+  .alert .form-switch .form-check-label {
+    background: transparent;
+  }
+  .alert .form-switch .form-check-label {
+    font-size: 0.9rem;
+  }
+  input[id^="switch"] {
+    box-shadow: none;
+  }
+  input[id^="switch"]:checked {
+    background-color: var(--primary);
+    border-color: var(--secondary);
   }
 `;
